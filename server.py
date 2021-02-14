001a340a34d64c0e3b050a7676fd521bd2df0741
Tyrus <tyrus.miles@gmail.com>
2021-02-13 20:15:30 -0700

    added the register command

--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 # join
 # save
 
-from socket import * 
+from socket import *
 import sys
 HOST = '127.0.0.1'
 serverPort = int(sys.argv[1])
@@ -13,34 +13,17 @@ serverPort = int(sys.argv[1])
 # So we own port ranges [40500,40999]
 # PORT = 40501
 
+registerList = {}
+
 contactList = None
 if(len(sys.argv) == 3):
-    contactList =  sys.argv[2] if (sys.argv[2] and sys.argv[2][-4:] == ".txt") else None
+    contactList = sys.argv[2] if (
+        sys.argv[2] and sys.argv[2][-4:] == ".txt") else None
 print("Contact list: {}\t".format(contactList), end="") if contactList else None
-print("PORT NUM: {}".format(serverPort))
-
-# command = input("Enter a command: ")
-
 serverSocket = socket(AF_INET, SOCK_DGRAM)
-serverSocket.bind(('', serverPort))
-print("The server is ready to receive")
-
-while True:
-    # print("hello")
-    message, clientAddress = serverSocket.recvfrom(2048)
-    # print(message.decode().upper())
-    realMsg = message.decode()
-    readCommand(realMsg)
-    modifiedMessage = message.decode().upper()
-    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
 
 
-
-
-
-
-
-def readCommand(command):
+def readCommand(command, clientAddr):
     command_split = command.split(" ")
     init = command_split[0].lower()
 
@@ -50,14 +33,24 @@ def readCommand(command):
         regIp = command_split[2]
         regPort = command_split[3]
 
+        adFormat = {
+            "name" : regContactName,
+            "ip" : regIp,
+            "port" : regPort
+        }
 
-        pass
+        if(regContactName not in registerList):
+            registerList[regContactName] = adFormat
+            serverSocket.sendto("SUCCESS".encode(),clientAddr)
+        else:
+            serverSocket.sendto("FAILURE".encode(), clientAddr)
+
+
 
     elif(init == "create"):
         print("You want to create something")
 
         conList = command_split[1]
-        pass
 
     elif(init == "query-lists"):
         # queries the server for the names of the contact lists
@@ -67,35 +60,43 @@ def readCommand(command):
     elif(init == "join"):
         contactListName = command_split[1]
         contactName = command_split[2]
-        pass
 
     elif(init == "leave"):
         contactListName = command_split[1]
         contactName = command_split[2]
-        pass
 
     elif(init == "exit"):
         contactName = command_split[1]
-        pass
 
     elif(init == "im-start"):
         contactListName = command_split[1]
         contactName = command_split[2]
-        pass
 
     elif(init == "im-complete"):
         contactListName = command_split[1]
         contactName = command_split[2]
 
-        pass
-
     elif(init == "save"):
         fileName = command_split[1]
-        pass
     else:
         print("Command not found")
 
+
 def main():
-    pass
+    print("PORT NUM: {}".format(serverPort))
+
+    serverSocket.bind(('', serverPort))
+    print("The server is ready to receive")
+
+    while True:
+        message, clientAddress = serverSocket.recvfrom(2048)
+        # print(message.decode().upper())
+        realMsg = message.decode()
+        readCommand(realMsg, clientAddress)
+        # modifiedMessage = message.decode().upper()
+        # serverSocket.sendto(modifiedMessage.encode(), clientAddress)
+
+main()
+
 
 
